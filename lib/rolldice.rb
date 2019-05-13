87183187373403581a12b63baf8e928bd1d2b0e5
Helen Hayward <helen.m.hayward@hotmail.co.uk>
2019-05-13 22:00:12 +0100

    Test Drive a Dice Class Completed - Unit tests and Code

--- lib/rolldice.rb
+++ lib/rolldice.rb
@@ -1,5 +1,9 @@
 class Dice
-  def roll
-    return rand(1..6)
+  def roll(dice = 1)
+    dice_array = []
+    dice.times do
+      dice_array << rand(1..6)
+    end
+    return dice_array
   end
 end
